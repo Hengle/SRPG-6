1fb509bcf8bc21dc3e6ca036762b9b3c7d1cda55
Ano88 <anoano0712@gmail.com>
2016-11-07 15:35:54 +0900

    キャラ移動思考中
    タップしたところに移動させたいが、できず。
    色々と調べたがNavigationを使う方法で実装できそうだが、マップを動的に生成している為かクリックしても動かず。

--- SRPG/Assets/Scripts/Chara/CharaMove.cs
+++ SRPG/Assets/Scripts/Chara/CharaMove.cs
@@ -1,13 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharaMove : MonoBehaviour {
+public class CharaMove : MonoBehaviour
+{
 
-	void Start () {
-	
-	}
-	
-	void Update () {
-	
-	}
+    private NavMeshAgent agent;
+    private RaycastHit hit;
+    private Ray ray;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
 }
